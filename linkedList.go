42e3861d99f6f4886567c6a313d60a0c24667426
Rob8150 <robert.clark.1967@gmail.com>
2022-08-19 11:41:56 +0930

    implement linked list in GO

---
+++ linkedList.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+type node struct {
+	data int
+	next *node
+}
+
+type linkedList struct {
+	head   *node
+	length int
+}
+
+func (l *linkedList) prepend(n *node) {
+	second := l.head
+	l.head = n
+	l.head.next = second
+	l.length++
+
+}
+
+func (l linkedList) printListData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next
+		l.length--
+	}
+	fmt.Printf("\n")
+}
+
+func (l *linkedList) deleteWithValue(value int) {
+
+	previousToDelete := l.head
+	for previousToDelete.next.data != value {
+		previousToDelete = previousToDelete.next
+	}
+	previousToDelete.next = previousToDelete.next.next
+	l.length--
+}
+
+func main() {
+	mylist := linkedList{}
+	node1 := &node{data: 48}
+	node2 := &node{data: 18}
+	node3 := &node{data: 16}
+	node4 := &node{data: 7}
+	node5 := &node{data: 9}
+	node6 := &node{data: 2}
+	mylist.prepend(node1)
+	mylist.prepend(node2)
+	mylist.prepend(node3)
+	mylist.prepend(node4)
+	mylist.prepend(node5)
+	mylist.prepend(node6)
+	mylist.printListData()
+	mylist.deleteWithValue(16)
+	mylist.printListData()
+
+}
